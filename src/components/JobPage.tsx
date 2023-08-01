291cdf232903e6a1c9caf513dc1a1bd39be48597
jteshale <101140557+jteshale@users.noreply.github.com>
2023-08-01 10:23:24 -0400

    Added Posting_ID (#18)

--- src/components/JobPage.tsx
+++ src/components/JobPage.tsx
@@ -96,6 +96,7 @@ const JobPage = ({
         {renderProperty('Preferred Educational/Experience', job.preferredEdExp)}
         {renderProperty('Required Documents', job.requiredDocuments)}
         {renderProperty('Additional Requirements', job.additionalReq)}
+        {renderProperty('Posting_ID', job.postingID)}
         {renderProperty('Application Instructions', job.applicationIn)}
         {applyLink ? 
         <Box mt={3}>
